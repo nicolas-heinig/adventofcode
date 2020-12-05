5c65348009905c97042412e75bb4b585a4c026c7
Nicolas Heinig <nicolas.heinig@gmail.com>
2020-12-05 20:00:37 +0100

    Add day 5

---
+++ day5/part1.rb
@@ -0,0 +1,17 @@
+input = File.readlines('day5/input.txt').map { _1.strip }
+
+to_binary = {
+  'F' => '0',
+  'B' => '1',
+  'L' => '0',
+  'R' => '1'
+}
+
+seats = input.each_with_object [] do |code, seats|
+  row = code[0..6].gsub(/\w/, to_binary).to_i 2
+  column = code[7..-1].gsub(/\w/, to_binary).to_i 2
+
+  seats << 8 * row + column
+end
+
+pp seats.max
